b68a1213d08b53cd5209dd9c76d14fe5b8206e2c
Abhishek Kumar <abhishekk580@gmail.com>
2021-06-27 13:16:27 +0530

    added login signup page

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,55 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import Link from "next/link";
 
 const Login = () => {
   return (
     <Layout>
-      <h1>Login</h1>
+      <main className="container">
+        <div className="col-md-6 offset-md-3">
+          <div className="card mt-5 shadow-sm">
+            <div className="m-4">
+              <h1 className="text-center">Login</h1>
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+
+                <div className="text-center">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </form>
+
+              <p>
+                Not registered?{" "}
+                <Link href="/signup">
+                  <a>Signup</a>
+                </Link>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,66 @@
 import React from "react";
 import Layout from "../components/Layout/Layout";
+import Link from "next/link";
 
 const SignUp = () => {
   return (
     <Layout>
-      <h1>Sign up</h1>
+      <main className="container">
+        <div className="col-md-6 offset-md-3">
+          <div className="card mt-5 shadow-sm">
+            <div className="m-4">
+              <h1 className="text-center">Signup</h1>
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputName1" className="form-label">
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleInputName1"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+
+                <div className="text-center">
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </form>
+
+              <p>
+                Already registered?{" "}
+                <Link href="/login">
+                  <a>Login</a>
+                </Link>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
